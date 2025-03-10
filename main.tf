d698497cc3223a2be91d16bf50e10db90c5e20e7
Ragil <work.ragil@gmail.com>
2025-03-11 04:28:49 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,31 @@
 provider "aws" {
-  region = "ap-southeast-3"  # Change to your preferred region
+  region = "ap-southeast-3"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "TerraformVPC"
+  }
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-southeast-3a"
+
+  tags = {
+    Name = "TerraformSubnet"
+  }
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID for your region
-  instance_type = "t2.micro"
+  ami           = "ami-06cfb597d8581294e"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.example.id
 
   tags = {
     Name = "TerraformEC2"
   }
-}
-
+} 
