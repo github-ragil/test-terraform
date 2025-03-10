b4a1ff6d19bcec6c4b5f4628658642771d985d76
Ragil <work.ragil@gmail.com>
2025-03-11 03:10:52 +0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformEC2"
+  }
+}
+
